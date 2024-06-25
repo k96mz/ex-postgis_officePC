18e7876b4585d880aad0b5bf8b914d650c10503c
k96mz <astrovation@gmail.com>
2024-06-25 17:14:38 -0400

    commit

--- spawn_p1.js
+++ spawn_p1.js
@@ -1,5 +1,32 @@
 const { spawn } = require('child_process');
 
+// 子プロセスのスクリプトを作成します
+const childScript = `
+  process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    const reversed = input.split('').reverse().join('');
+    process.stdout.write(reversed + '\\n');
+  });
+`;
+
+// 子プロセスを作成して、標準入出力を設定します
+const child = spawn('node', ['-e', childScript], {
+  stdio: ['pipe', 'pipe', 'inherit'],
+});
+
+// 親プロセスから子プロセスにデータを送信します
+child.stdin.write('Hello, world!\n');
+
+// 子プロセスからの出力を処理します
+child.stdout.on('data', data => {
+  console.log(`子プロセスからの出力: ${data}`);
+});
+
+// 子プロセスの終了を処理します
+child.on('close', code => {
+  console.log(`子プロセスが終了しました。終了コード: ${code}`);
+});
+
 /*
 // PowerShellコマンドを実行
 const ls = spawn('powershell.exe', [
